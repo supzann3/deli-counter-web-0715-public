3627737e0fa44bfe7fe0facefbdcc69e4ef15d2a
supzann3 <susan.hehe01@gmail.com>
2015-07-31 20:01:53 -0400

    add deli counter method

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+katz_deli = []
+
+def take_a_number(array,name)
+  array.push(name)
+  position=array.index(name)
+  puts "Welcome, #{name}. You are number #{position+1} in line."
+end
+
+
+def now_serving(array)
+  if array==[]
+    puts "There is nobody waiting to be served!"
+  else
+      current=array.first
+      puts "Currently serving #{current}."
+  new_array=array.shift
+  end
+end
+
+
+def line(array)
+  combination=""
+if array==[]
+    puts "The line is currently empty."
+  else
+  array.each.with_index(1) do |name,number|
+     combination << " #{number}. #{name}"
+  end
+  puts "The line is currently:#{combination}"
+  end
+end
+
